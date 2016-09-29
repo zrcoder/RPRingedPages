5491aed815b13ff52de1a65a61f6d000ea884254
admin <love-nankai@163.com>
2016-09-29 17:08:23 +0800

    ～

--- RPRingedPages/ViewController.m
+++ RPRingedPages/ViewController.m
@@ -49,7 +49,7 @@
 }
 
 - (void)makeDataSource {
-    for (int i=0; i<3; i++) {
+    for (int i=0; i<7; i++) {
         NSString *s = [NSString stringWithFormat:@"%c", i + 'A'];
         [self.dataSource addObject:s];
     }
